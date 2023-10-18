f23756b380060c8b2e7a38133951ed80184dff68
kenippert <89279599+kenippert@users.noreply.github.com>
2023-10-18 12:32:53 -0400

    AddSEM
    Standard error of the mean calculation for latency and number of set shifts

--- DataAnalysisSetShifting.m
+++ DataAnalysisSetShifting.m
@@ -4,6 +4,10 @@ numberSetShifts = calculateAverage(data_dictionary,"AttentionalSetsCompleted");
 disp("AttentionalSetsCompleted");
 printDictionaryData(numberSetShifts);
 
+attentionalSetSEM = calculateSEM(data_dictionary,"AttentionalSetsCompleted");
+disp("AttentionalSetSEM");
+printDictionaryData(attentionalSetSEM);
+
 trialperformance = calculateAverageTrialPerf(data_dictionary);
 disp("TrialPerformance");
 printDictionaryData(trialperformance);
@@ -12,8 +16,11 @@ printDictionaryData(trialperformance);
 latency = calculateAverage(data_dictionary,"Latency");
 disp("Latency");
 printDictionaryData(latency);
-%the code above accesses the value of a variable on a given trial in a
-%given sessionf or a given mouse
+
+latencySEM = calculateSEM(data_dictionary,"Latency");
+disp("latencySEM");
+printDictionaryData(latencySEM);
+
 
 function avgDict = calculateAverage(dictParam, key)
     
@@ -46,9 +53,46 @@ function avgDict = calculateAverage(dictParam, key)
             % the end of temporary array so the index will be the same
             tempArray{length(tempArray) + 1} = mean(...
                 sessionArray, "omitnan");
-
             % add new tempArray back to its spot in avgDict
             avgDict{dictionaryKeys(i)} = tempArray;
+        end
+    end
+end
+
+
+function semDict = calculateSEM(dictParam, key)
+    
+    semDict = dictionary();
+    % gets the dictionary keys for the dictionary
+    % i.e. returns [136,137,138,...]
+    dictionaryKeys = keys(dictParam);
+    % iterate througuh mouse sessions (i.e. 137,138,...)
+    for i = 1:size(dictionaryKeys)
+        % iterate through sessions per mouse (i.e. 137 session 1, 137
+        % session 2, ...)
+        mouseArray = dictParam{dictionaryKeys(i)};
+        for j = 1:length(mouseArray)
+            % gets the average of the sectionToCalculate (i.e.
+            % TrialPerformance)
+            sessionDict = mouseArray(j);
+            sessionDict = sessionDict{1};
+
+            sessionArray = sessionDict(key);
+            sessionArray = sessionArray{1};
+            tempArray = {};
+            % first check if avgDict is initialized otherwise will error
+            if isConfigured(semDict)
+                % then check if specific part of avgDict is initialized
+                if isKey(semDict, dictionaryKeys(i))
+                    tempArray = semDict{dictionaryKeys(i)};
+                end
+            end
+            % adding mean of sessionDict's session to Calculate to
+            % the end of temporary array so the index will be the same
+            tempArray{length(tempArray) +1} = (std(sessionArray,"omitmissing"))/(sqrt(length(sessionArray)));
+
+            % add new tempArray back to its spot in avgDict
+            semDict{dictionaryKeys(i)} = tempArray;
         end
     end
 end
@@ -108,7 +152,6 @@ function avgDict = calculateAverageTrialPerf(dictParam)
         end
     end
 end
-
 
 
 function printDictionaryData(dict)
